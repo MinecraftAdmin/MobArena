426e76202c3096ededc6326df9da41a307992190
MinecraftAdmin <furquanatique@gmail.com>
2014-02-05 07:34:36 -0600

    Update Messenger.java

--- src/com/garbagemule/MobArena/Messenger.java
+++ src/com/garbagemule/MobArena/Messenger.java
@@ -25,7 +25,7 @@ public class Messenger
         }
 
         // Otherwise, send the message with the [MobArena] tag.
-        p.sendMessage(ChatColor.GREEN + "[MobArena] " + ChatColor.RESET + msg);
+        p.sendMessage(ChatColor.YELLOW + "[Grinder] " + ChatColor.RESET + msg);
         return true;
     }
 
